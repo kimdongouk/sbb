2327a81dedb56764d6c3d14b7a009b78295adbab
kimdongouk <kidyac1@gmail.com>
2023-06-16 18:17:04 +0900

    new

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,48 @@
+package com.mysite.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private AnswerRepository answerRepository;
+	
+	@Transactional
+	@Test
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(4);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		System.out.println(q.getContent());
+		
+		List<Answer> answerList = q.getAnswerList();
+		System.out.println(answerList);
+		
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		System.out.println("@!@!  " + answerList.get(0).getContent());
+	}
+
+}
